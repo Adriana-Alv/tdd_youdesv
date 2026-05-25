01a66fbbb35c2e882b144fda10fde9d15d7d4a57
Adriana-Alv <202214234g@utea.edu.pe>
2026-05-25 17:28:40 -0500

    Creamos EventController

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function store(Request $request)
+    {
+        return redirect()->route('events.index');
+    }
+
+    
+}
