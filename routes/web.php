7ccd4ed8f2f91560f560771e1c1e9bd687816c8f
Adriana-Alv <202214234g@utea.edu.pe>
2026-05-25 17:28:55 -0500

    Modificamos la web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/events', [EventController::class, 'index'])->name('events.index');
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
