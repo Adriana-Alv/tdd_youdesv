db132992702b2dd1d4b1eca35bf9c4b49189cd5d
Adriana-Alv <202214234g@utea.edu.pe>
2026-05-25 16:43:49 -0500

    Prueba de error (302)

---
+++ tests/Feature/CreateEventTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Carbon\Carbon;
+
+class CreateEventTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_an_event_can_be_created(): void
+    {
+        //Arrange:
+        $eventData = [
+            'name' => 'Conferencia de YouDevs',
+            'featured' => 'meme.png',
+            'date' => Carbon::now(),
+            'time' => '12:00:00',
+            'location' => 'El Santiago de Cusco'
+
+        ];
+        //Act
+        $response = $this->post('/events', $eventData);
+        //Assert
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('events', $eventData);
+    }
+}
