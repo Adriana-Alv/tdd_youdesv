b218bd51758c1ab1b33f64b348520b6d6ae30c4e
Adriana-Alv <202214234g@utea.edu.pe>
2026-05-25 17:56:21 -0500

    Refactor - validar los datos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function store(Request $request)
     {
+        $eventData = $request->validate([
+            'name' => 'required|string|max:60',
+            'featured' => 'required|string|max:50',
+            'date' => 'required|date',
+            'time' => 'required|date_format:H:i:s',
+            'location' => 'required|string|max:60',
+        ]);
+
+        Event::create($eventData);
+
         return redirect()->route('events.index');
     }
 
-    
+    public function index()
+    {
+        return 'Lista de eventos';
+    }
 }
