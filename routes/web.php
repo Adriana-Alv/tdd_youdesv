4068441f181cbbb77345af2ac7e4b68ac7a5efc7
Adriana-Alv <202214234g@utea.edu.pe>
2026-05-25 19:37:15 -0500

    Rutas del CRUD

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/events', [EventController::class, 'index'])->name('events.index');
 Route::post('/events', [EventController::class, 'store'])->name('events.store');
+
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
